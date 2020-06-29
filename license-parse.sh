e35df6a043d74e11471ca0ef66771cc0d7f20501
Ben-401 <ben.0x0401@gmail.com>
2020-06-29 17:18:41 +0930

    updated script to process the SCRipt-files, added de-duplicate data-file, #244
    the dedup data-file, it needs more entries, but this works for a start.
    
    currently the script does apply a standard LGPL-3.0-or-later licence to identified scripts

--- license-parse.sh
+++ license-parse.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script is intended to automate the implementation of the REUSE specification
+# to the files in the mega65-core repo,
+
 # we add a prefix to each of the output files (to group them together)
 PREFNAME="LICPARSE"
 
@@ -23,8 +26,9 @@ IGN_PATHS="^./.git/\|^./ipcore_dir/\|^./src/_unused/\|^./src/_unused2/\|^./KickA
 #
 cat ${PREFNAME}.2.sorted | grep -v $IGN_PATHS > ${PREFNAME}.3.trimmed
 #
-echo "Removed the following"
-diff ${PREFNAME}.2.sorted ${PREFNAME}.3.trimmed
+# DEBUG
+#echo "Removed the following"
+#diff ${PREFNAME}.2.sorted ${PREFNAME}.3.trimmed
 
 
 # output file #3 contains URLs of files to process
@@ -76,7 +80,7 @@ done;
 ###################################################
 ###################################################
 
-if [ 1 == 1 ]; then
+if [ 0 == 1 ]; then
   echo "Processing the TXT's: ${KNOWN_TXT_EXTNS}"
   #
   cat ${PREFNAME}.4.txt | while read thisfile; do
@@ -90,7 +94,7 @@ fi
 ###################################################
 ###################################################
 
-if [ 1 == 1 ]; then
+if [ 0 == 1 ]; then
   #
   echo "Processing the BIN's: ${KNOWN_BIN_EXTNS}"
   #
@@ -109,9 +113,104 @@ if [ 1 == 1 ]; then
   echo "Processing the SCR's: ${KNOWN_SCR_EXTNS}"
   #
   cat ${PREFNAME}.4.scr | while read thisfile; do
-    echo "$thisfile"
+    echo "==== Processing: $thisfile"
+    #
+    # check for hashbang, as we want the header to go BELOW the first line
+    #
+    FIRSTLINE=`head -n 1 $thisfile`
+    echo "firstline=\"${FIRSTLINE}\""
+    #
+    if [[ "$FIRSTLINE" =~ ^#! ]] ; then
+      echo "detected hashbang"
+      HASHBANG="Y"
+    else
+      echo "no hashbang"
+      HASHBANG="N"
+    fi
+    #
+    # DEBUG
+    #head $thisfile
+    #
+    echo "===="
+
+    # for scripts with hashbang, keep the first line
+    # then add LICENCEHEADER
+    if [[ $HASHBANG == "Y" ]] ; then
+      #
+      echo $FIRSTLINE > ${thisfile}.temp
+    fi
+
+    # construct the LICENCEHEADER template (specific to SCR-files, ie use HASH for comment)
+    #
+    LICENCEHEADER_SCR="#\n# SPDX-FileCopyrightText: 2020 MEGA\n#\n# Contributors:\n# __CONTRIBUTORS__\n#\n# SPDX-License-Identifier: LGPL-3.0-or-later\n#"
+    #
+    # DEBUG
+    echo -e $LICENCEHEADER_SCR
+    echo "==^^ LICENCEHEADER_SCR"
+    #
+    # write out a temporary file
+    #
+    echo -e $LICENCEHEADER_SCR > ${thisfile}.temp.header
+
+
+
+    # get a list of the contributors
+    #
+    # 1. get a complete git-log of the file, and pull out the string "Author:..." (git grep)
+    # 2. normalise the list where an author has multiple names and/or email addresses (sed)
+    # 3. de-duplicate the names depending on the rules file (awk)
+    CONTRIBUTORS=`git log $thisfile | grep Author | sed -f ./license-parse-dedup.dat | awk '!a[$0]++' `
+    #
+    # DEBUG
+    echo "git log <all>:"
+    git log $thisfile | grep Author
+    echo "and the processed:"
+    echo "$CONTRIBUTORS"
+    echo "==^^CONTRIBUTORS"
+    #
+    # write out a temporary file
+    #
+    echo -e "$CONTRIBUTORS" > ${thisfile}.temp.contrib
+    #
+    # now, remove the leading "Author:   " in each entry, and align as appropriate
+    sed -i 's/Author: /#   /g' ${thisfile}.temp.contrib
+    #
+
+    # now join the LICENCEHEADER with the CONTRIBUTORS
+    # 1. top of LICENCEHEADER downto "__CONTRIBUTORS__"
+    # 2. contents of CONTRIBUTORS
+    # 3. botton of LICENCEHEADER starting after "__CONTRIBUTORS__"
+    #
+    # 1: "grep -B" says 9999 lines BEFORE the matched text (ie the top of the file downto match)
+    #    "head -1" removes the matched line
+    cat ${thisfile}.temp.header | grep -B 9999 "__CONTRIBUTORS__" | head -n -1 >> ${thisfile}.temp
+    # 2: all of the contributors
+    cat ${thisfile}.temp.contrib >> ${thisfile}.temp
+    # 3: "grep -A" says 9999 lines AFTER the matched text (ie the bottom of the file upto match)
+    #    "tail -1" removes the matched line
+    cat ${thisfile}.temp.header | grep -A 9999 "__CONTRIBUTORS__" | tail -n +2 >> ${thisfile}.temp
+
+    # now place the rest of the contents of the original file into this temp-file
+    #
+    # for the SCR-files with HASHBANG, we have already put in the first line, so just lines 2-onwards
+    if [[ $HASHBANG == "Y" ]] ; then
+      cat ${thisfile} | tail -n +2 >> ${thisfile}.temp
+    else
+      cat ${thisfile}              >> ${thisfile}.temp
+    fi
+
+    # and now replace the original file with the temp file
+    mv ${thisfile}.temp ${thisfile}
+    #rm ${thisfile}.temp
+
+    # remove temporary files
+    rm ${thisfile}.temp.header
+    rm ${thisfile}.temp.contrib
+
   done
   echo " "
+
+
 fi
 
 ###################################################
